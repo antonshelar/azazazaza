e6355e078bf0e9c73656f232f1b5d76705266d8b
antonshelar <antonshelar@gmail.com>
2016-01-16 22:01:33 +0600

    added pool

--- src/pool.c
+++ src/pool.c
@@ -24,11 +24,61 @@ int add_to_pool(struct pool *pool, void *ptr){
 		pool->elements = (struct pool_element *)malloc(sizeof(struct pool_element));
 		if(!pool->elements)
 			return -1;
+
+		pool->elements->ptr = ptr;
+		pool->elements->next = NULL;
+
+		pool->elements_count = 1;
+
+		return 0;
+
 	}
+
+	struct pool_element *element = pool->elements;
+
+	while(element->next)
+		element = element->next;
+
+	element->next = (struct pool_element *)malloc(sizeof(struct pool_element));
+
+	element = element->next;
+	element->ptr = ptr;
+	element->next = NULL;
+
+	pool->elements_count++;
+
+	return 0;
+
 
 }
 
 void free_pool(struct pool *pool){
+
+	if(!pool)
+		return;
+
+	if(!pool->elements){
+
+		free(pool);
+
+		return;
+	}
+
+	struct pool_element *element = pool->elements;
+	struct pool_element *prev_element;
+
+	while(element){
+
+		free(element->ptr);
+
+		prev_element = element;
+		element = prev_element->next;
+
+		free(prev_element);
+
+	}
+
+	free(pool);
 
 
 }
